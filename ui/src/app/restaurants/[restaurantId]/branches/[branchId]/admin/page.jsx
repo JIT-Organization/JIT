3772dc50a0e65014de350ac47572645950b1039e
Dhileepan <dhileepansendil@gmail.com>
2025-01-29 17:06:46 +0530

    FoodCard, menu and the UI structure

---
+++ ui/src/app/restaurants/[restaurantId]/branches/[branchId]/admin/page.jsx
@@ -0,0 +1,12 @@
+
+
+export default function AdminBranch() {
+    return (
+      <div>
+        <h2>
+          Admin page for a single branch
+        </h2>
+  
+      </div>
+    )
+  }
---
+++ ui/src/app/restaurants/[restaurantId]/page.jsx
@@ -0,0 +1,19 @@
+import Components from "@/components/Components";
+//import { useRouter } from 'next/router';
+//import { usePathname } from 'next/navigation';
+
+export default async function Restaurant({ params }) {
+    // const router = useRouter();
+    // const { restaurantId } = router.query;
+    // const pathname = usePathname();
+    // const restaurantId = pathname.split('/').pop();
+    const { restaurantId } = await params;
+  return (
+    <div>
+      <h2>
+        Restaurant page for a particular restaurant identified by a restaurantId
+      </h2>
+      <p className="text-lg">You are viewing details for restaurant with ID: {restaurantId}</p>
+    </div>
+  )
+}
