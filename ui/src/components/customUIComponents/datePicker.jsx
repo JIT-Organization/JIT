a8865c7099710bb56dbee9af45b80492f74b912d
user-lee-005 <93381862+user-lee-005@users.noreply.github.com>
2025-02-08 16:46:53 +0530

    Added a table and the example columns to it

--- ui/src/components/customUIComponents/datePicker.jsx
+++ ui/src/components/customUIComponents/datePicker.jsx
@@ -14,7 +14,7 @@ import {
 } from "@/components/ui/popover"
 
 export function DatePickerDemo() {
-  const [date, setDate] = React.useState<Date>(null)
+  const [date, setDate] = React.useState(null)
 
   return (
     <Popover>
