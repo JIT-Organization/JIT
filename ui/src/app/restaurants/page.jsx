264bfa8589d5fb9733535333b59f85d7fefaeae1
DhileepanUTF <85431148+DhileepanUTF@users.noreply.github.com>
2025-02-15 10:34:43 +0530

    FoodCard, MEnu and the UI structure (#49)
    * Food component for users
    
    * Used custom components
    
    * Food component for users
    
    * FoodCard, menu and the UI structure
    
    * Altered structure

--- ui/src/app/restaurants/page.jsx
+++ ui/src/app/restaurants/page.jsx
@@ -1,10 +1,10 @@
 import Components from "@/components/Components";
 
-export default function Restaurant() {
+export default function Restaurants() {
   return (
     <div>
       <h2>
-        Restaurant page
+        ALL Restaurants page
       </h2>
       <Components />
     </div>
