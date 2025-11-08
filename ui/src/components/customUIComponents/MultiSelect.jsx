0c78ae29a72604730d6ff3e1d349f9e34d5692c9
user-lee-005 <93381862+user-lee-005@users.noreply.github.com>
2025-11-08 16:58:16 +0530

    Z index fix

--- ui/src/components/customUIComponents/MultiSelect.jsx
+++ ui/src/components/customUIComponents/MultiSelect.jsx
@@ -1,8 +1,10 @@
+'use client'
 import { Popover, PopoverTrigger, PopoverContent } from "../ui/popover";
 import { Command, CommandInput, CommandList, CommandItem } from "../ui/command";
 import { Check } from "lucide-react";
 import { Button } from "../ui/button";
 import { cn } from "@/lib/utils";
+import { useRef } from "react";
 
 export default function MultiSelect({
   options,
@@ -48,64 +50,66 @@ export default function MultiSelect({
   const isAllSelected = options && options.length > 0 && (value || []).length > 0 && options.every(opt => (value || []).includes(opt.value));
 
   return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <Button
-          variant="outline"
-          className={cn(
-            "w-full justify-start text-left",
-            disabled && "opacity-50 cursor-not-allowed pointer-events-none",
-            className
-          )}
-          disabled={disabled}
-        >
-          <span className={(value || []).length ? "truncate" : "text-muted-foreground"}>
-            {(value || []).length
-              ? options
-                .filter((opt) => (value || []).includes(opt.value))
-                .map((opt) => opt.label)
-                .join(isSingleSelect ? "" : ", ")
-              : placeholder}
-          </span>
-        </Button>
-      </PopoverTrigger>
-      {!disabled && (
-        <PopoverContent className="w-full p-0 bg-white">
-          <Command>
-            <CommandInput placeholder="Search options..." />
-            <CommandList>
-              {!isSingleSelect && showAllOption && (
-                <CommandItem
-                  key="all"
-                  onSelect={() => toggleOption("all")}
-                  className="cursor-pointer"
-                >
-                  <span className="flex-1">All</span>
-                  {isAllSelected && (
-                    <Check className="ml-auto h-4 w-4" />
-                  )}
-                </CommandItem>
-              )}
-              {options.map((option, index) =>
-                option.value ? (
+    <div>
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            className={cn(
+              "w-full justify-start text-left",
+              disabled && "opacity-50 cursor-not-allowed pointer-events-none",
+              className
+            )}
+            disabled={disabled}
+          >
+            <span className={(value || []).length ? "truncate" : "text-muted-foreground"}>
+              {(value || []).length
+                ? options
+                  .filter((opt) => (value || []).includes(opt.value))
+                  .map((opt) => opt.label)
+                  .join(isSingleSelect ? "" : ", ")
+                : placeholder}
+            </span>
+          </Button>
+        </PopoverTrigger>
+        {!disabled && (
+          <PopoverContent container={false} className="w-full p-0 bg-white relative z-[9999]">
+            <Command>
+              <CommandInput placeholder="Search options..." />
+              <CommandList>
+                {!isSingleSelect && showAllOption && (
                   <CommandItem
-                    key={option.value + index}
-                    onSelect={() => toggleOption(option.value)}
+                    key="all"
+                    onSelect={() => toggleOption("all")}
                     className="cursor-pointer"
                   >
-                    <span className="flex-1">{option.label}</span>
-                    {(value || []).includes(option.value) && (
+                    <span className="flex-1">All</span>
+                    {isAllSelected && (
                       <Check className="ml-auto h-4 w-4" />
                     )}
                   </CommandItem>
-                ) : (
-                  <CommandItem key={option.label}>{option.label}</CommandItem>
-                )
-              )}
-            </CommandList>
-          </Command>
-        </PopoverContent>
-      )}
-    </Popover>
+                )}
+                {options.map((option, index) =>
+                  option.value ? (
+                    <CommandItem
+                      key={option.value + index}
+                      onSelect={() => toggleOption(option.value)}
+                      className="cursor-pointer"
+                    >
+                      <span className="flex-1">{option.label}</span>
+                      {(value || []).includes(option.value) && (
+                        <Check className="ml-auto h-4 w-4" />
+                      )}
+                    </CommandItem>
+                  ) : (
+                    <CommandItem key={option.label}>{option.label}</CommandItem>
+                  )
+                )}
+              </CommandList>
+            </Command>
+          </PopoverContent>
+        )}
+      </Popover>
+    </div>
   );
 }
